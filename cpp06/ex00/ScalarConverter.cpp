2a7329abba3798f619a73d2a75a368771687941d
Abdelaziz Aboudrar <aaboudra@e1r5p15.1337.ma>
2026-06-27 21:13:06 +0100

    aalmost

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,4 +1,5 @@
 #include "ScalarConverter.hpp"
+#include <limits>
 
 enum e_type
 {
@@ -125,34 +126,146 @@ e_type detectType(const std::string& str)
     return INVALID;      
 }
 
+void print_double(double d, bool unvalid)
+{
+
+    if(unvalid)
+        std::cout << "double : impossible" << std::endl;
+    else
+        std::cout << "double : " << d << std::endl;
+}
+
+void print_float(float f, bool unvalid)
+{
+    if(unvalid)
+        std::cout << "float : impossible" << std::endl;
+    else
+        std::cout << "float : " << f << std::endl;
+}
+
+void print_int(int i, bool unvalid)
+{
+    if(unvalid)
+        std::cout << "int : impossible" << std::endl;
+    else
+        std::cout << "int : " << i << std::endl;
+}
+
+void print_char(char c, bool unvalid)
+{
+    if (unvalid)
+    {
+        std::cout << "char: impossible\n";
+        return;
+    }
+    if(isprint(c))
+        std::cout << "char : '" << c << "'" << std::endl;
+    else 
+        std::cout << "char : Non displayable" << std::endl;
+}
+
+void handel_char(const std::string input)
+{
+    //char
+    char c = input[0];
+    print_char(c, false);
+    //int
+    int i = static_cast<int>(c);
+    print_int(i, false);
+    //flout 
+    float f = static_cast<float>(c);
+    print_float(f, false);
+    //double
+    double d = static_cast<double>(c);
+    print_double(d, false);
+}
+
+void handel_int(long value)
+{
+    // char
+    if (value < std::numeric_limits<char>::min() ||
+        value > std::numeric_limits<char>::max())
+    {
+        print_char(0, true);
+    }
+    else
+    {
+        char c = static_cast<char>(value);
+        print_char(c, false);
+    }
+    // int
+    if (value < std::numeric_limits<int>::min() ||
+        value > std::numeric_limits<int>::max())
+    {
+        print_int(0, true);
+    }
+    else
+    {
+        int i = static_cast<int>(value);
+        print_int(i, false);
+    }
+    // float
+    float f = static_cast<float>(value);
+    print_float(f, false);
+    // double
+    double d = static_cast<double>(value);
+    print_double(d, false);
+}
+
+void handel_float(long f)
+{
+    
+}
+
 void ScalarConverter::convert(const std::string &input)
 {
+
     switch(detectType(input))
     {
         case CHAR:
-            char c = input[0];
-            std::cout << c << std::endl;
-            int i = static_cast<int>(c);
-            double d = static_cast<double>(c);
-            float f = static_cast<float>(c);
-
-
-
+            handel_char(input);
             break;
 
         case INT:
-        
+            char *end;
+            long value = std::strtol(input.c_str(), &end, 10);
+            handel_int(value);
             break;
 
         case FLOAT:
-        
+            char *end;
+            long value = std::strtol(input.c_str(), &end, 10);
+            handel_float(value);
             break;
 
         case DOUBLE:
         
             break;
-
         
+        case PSEUDO:
+            if (input == "nan" || input == "nanf") {
+		        std::cout << "char: impossible" << std::endl;
+		        std::cout << "int: impossible" << std::endl; 
+		        std::cout << "float: nanf" << std::endl; 
+		        std::cout << "double: nan" << std::endl; 
+		        
+	            }
+	        else if (input == "+inf" || input == "+inff") { 
+	        	std::cout << "char: impossible" << std::endl; 
+	        	std::cout << "int: impossible" << std::endl; 
+	        	std::cout << "float: +inff" << std::endl; 
+	        	std::cout << "double: +inf" << std::endl; 
+	        	 
+	            } 
+	        else if (input == "-inf" || input == "-inff") { 
+	        	std::cout << "char: impossible" << std::endl; 
+	        	std::cout << "int: impossible" << std::endl; 
+	        	std::cout << "float: -inff" << std::endl; 
+	        	std::cout << "double: -inf" << std::endl; 
+	        	
+	            }
+            break;
+      
     }
     
 
